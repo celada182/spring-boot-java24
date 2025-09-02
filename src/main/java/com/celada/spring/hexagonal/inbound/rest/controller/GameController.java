cec427bf3b96af1db1cbc9e1ccebd6758d30b546
Javier Celada <celada.jcm.javier@gmail.com>
2025-09-02 11:15:44 +0200

    Refactor to Maven

--- src/main/java/com/celada/spring/hexagonal/inbound/rest/controller/GameController.java
+++ src/main/java/com/celada/spring/hexagonal/inbound/rest/controller/GameController.java
@@ -1,6 +1,19 @@
 package com.celada.spring.hexagonal.inbound.rest.controller;
 
 import com.celada.openapi.api.GameApi;
+import com.celada.openapi.model.ApiGame;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public class GameController implements GameApi {
+    @Override
+    public ResponseEntity<Void> createGame(ApiGame apiGame) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<ApiGame>> getGames() {
+        return null;
+    }
 }
